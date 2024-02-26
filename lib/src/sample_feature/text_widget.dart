70c400bc536500a00e3ed8edc28c99e9cce9bfae
Joaquin Duran <duranjoa20@gmail.com>
2024-02-26 12:28:09 -0300

    Changed the possition of the X

--- lib/src/sample_feature/text_widget.dart
+++ lib/src/sample_feature/text_widget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:party_games/src/first_game/Servicies/game_sentence.dart';
 import 'package:party_games/src/first_game/Servicies/local_text_service.dart';
 
-
 class TextDisplayWidget extends StatefulWidget {
   final LocalTextService textService;
 
@@ -24,13 +23,14 @@ class _TextDisplayWidgetState extends State<TextDisplayWidget> {
 
   Future<void> _loadTexts() async {
     try {
-      _texts = await  widget.textService.fetchTexts();
+      _texts = await widget.textService.fetchTexts();
       setState(() {});
     } catch (e) {
       print(e);
       // Handle errors or show a message to the user
     }
   }
+
   void _showNextText() {
     setState(() {
       if (_currentIndex < _texts.length - 1) {
@@ -48,63 +48,71 @@ class _TextDisplayWidgetState extends State<TextDisplayWidget> {
   }
 
   Color _getBackgroundColorForSentenceType() {
-  if (_currentIndex == 0) {return Colors.white;}
-  var text = _texts[_currentIndex];
-  switch (text.type) {
-    case SentenceType.virus:
-      return Colors.redAccent; // Example color for 'virus' type
-    case SentenceType.text:
-      return Colors.white; // Example color for 'text' type
-    case SentenceType.challenge:
-      return Colors.blueAccent; // Example color for 'challenge' type
-    default:
-      return Colors.white; // Default color
+    if (_currentIndex == 0) {
+      return Colors.white;
+    }
+    var text = _texts[_currentIndex];
+    switch (text.type) {
+      case SentenceType.virus:
+        return Colors.redAccent; // Example color for 'virus' type
+      case SentenceType.text:
+        return Colors.white; // Example color for 'text' type
+      case SentenceType.challenge:
+        return Colors.blueAccent; // Example color for 'challenge' type
+      default:
+        return Colors.white; // Default color
+    }
   }
-}
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Party Game'), // Provides a title for your game page
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () => Navigator.of(context)
-              .pop(), // Ensures back navigation is possible
-        ),
-      ),
       backgroundColor: _getBackgroundColorForSentenceType(),
-      body: _texts.isEmpty
-          ? const Center(child: CircularProgressIndicator())
-          : Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    _texts[_currentIndex].text,
-                    style: const TextStyle(
-                        fontSize: 24, fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: Stack(
+        children: [
+          // This part represents the main content of your screen.
+          _texts.isEmpty
+              ? const Center(child: CircularProgressIndicator())
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    ElevatedButton(
-                      onPressed: _showPreviousText,
-                      child: const Text('Previous'),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        _texts[_currentIndex].text,
+                        style: const TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
                     ),
-                    ElevatedButton(
-                      onPressed: _showNextText,
-                      child: const Text('Next'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        ElevatedButton(
+                          onPressed: _showPreviousText,
+                          child: const Text('Previous'),
+                        ),
+                        ElevatedButton(
+                          onPressed: _showNextText,
+                          child: const Text('Next'),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
+          // Positioned X button in the top right corner.
+          Positioned(
+             // Adjust for the status bar height.
+            right: 0, // Align to the right side of the screen.
+            child: SafeArea(
+              child: IconButton(
+                icon: const Icon(Icons.close, size: 30.0), // X icon.
+                onPressed: () => Navigator.of(context).pop(),
+              ),
             ),
+          ),
+        ],
+      ),
     );
   }
 }
-
